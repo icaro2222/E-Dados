b89fa3f9a0ddfe1ed58c66de2936b93765fcbf26
icaro2222 <icarodias2222@gmail.com>
2022-11-08 21:12:54 -0300

    make it better

--- edados/view/contraste/view_contrast_quest_socio_notas.py
+++ edados/view/contraste/view_contrast_quest_socio_notas.py
@@ -184,9 +184,9 @@ def contrast_quest_socio_notas(request):
         # plt.show(figura)
         buffer = BytesIO()
         savefig = plt.savefig(buffer, format='png', facecolor='#e8eeff')
-        nome_do_relatorio = 'dados_relatorio/' + str(uuid.uuid4()) + '.pdf'
-        nome_destino_do_relatorio = str(BASE_DIR) + '/static/' + nome_do_relatorio
-        plt.savefig(fname=nome_destino_do_relatorio, format='pdf', facecolor='#e8eeff')
+        # nome_do_relatorio = 'dados_relatorio/' + str(uuid.uuid4()) + '.pdf'
+        # nome_destino_do_relatorio = str(BASE_DIR) + '/static/' + nome_do_relatorio
+        # plt.savefig(fname=nome_destino_do_relatorio, format='pdf', facecolor='#e8eeff')
         # plt.savefig(fname='dados/Relatório comparativo entre Questões Socioeconômicas e Desempenho no Enem.pdf' , format='pdf')
         buffer.seek(0)
         image_png = buffer.getvalue()
