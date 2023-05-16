15bdec1c64405294cba847c054ab5bf8a242eaf6
icaro2222 <icarodias2222@gmail.com>
2023-05-16 15:53:57 -0300

    refatorando códigos

--- edados/urls.py
+++ edados/urls.py
@@ -15,11 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-
-from .view.outros import view_quest_demo_notas
-# from .view import view_quest_socio_notas, view_quest_demo_socioe, view_quest_socio_notas_sexo , view_quest_socio_notas_deficiencia, view_regiao, view_regiao_mapa#, views, view,  view_plot
-from .view.contraste import view_contrast_quest_socio_notas
 from .view.dashboard import dashboard
 from .view.aba_de_informacoes import view_aba_de_informacoes
 from .view.fomulario_1 import view_formulario_1, view_formulario_1_4
@@ -48,18 +43,4 @@ urlpatterns = [
     path('Correcoes/', view_aba_de_informacoes.correcoes_bugs, name="correcoes"),
     path('Dicionário Microdados/', view_aba_de_informacoes.dicionario_microdados, name="dicionario"),
     
-    # path('teste/', views.teste, name='teste'),
-    # path('manipulando/', views.index, name='page_dados'),
-    # path('grafico_tabela/', view.Grafico_Tabela, name="grafico_tabela"),
-    # path('grafico_plot_teste/', view_plot.Grafico_Plot_Teste, name="grafico_plot_teste"),
-    # path('Quest Soc Notas/', view_quest_socio_notas.view_quest_socio_notas, name="Quest_Soc_Notas"),
-    # path('Quest Demograf Notas/', view_quest_demo_notas.view_quest_demo_notas, name="Quest_Dem_Notas"),
-    # path('Questão_Demográfica_vs_Socioeconômica/', view_quest_demo_socioe.view_quest_demo_socioe, name="Quest_Dem_Socioe"),
-    # path('Desenpenho por Regiao/', view_regiao.regiao, name="Desenpenho_por_Regiao"),
-    # path('Desenpenho por Regiao Mapa/', view_regiao_mapa.view_regiao_mapa, name="Desenpenho_por_Regiao_Mapa"),
-    # path('Quest Soc Notas Sexo/', view_quest_socio_notas_sexo.Quest_Soc_Notas_Sexo, name="Quest_Soc_Notas_Sexo"),
-
-    # URLs de Contraste entre Deficiêntes e Não deficiêntes
-    # path('Contrast Quest Soc Notas/', view_contrast_quest_socio_notas.contrast_quest_socio_notas, name="Contrast_Quest_Soc_Notas_Deficiencia"),
-
 ]
