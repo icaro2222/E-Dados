dc0ebd1881724df662ba13a2bcdb7cbd3fc48a27
icaro2222 <icarodias2222@gmail.com>
2023-06-03 15:32:42 -0300

    correção de erros

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,15 +3,16 @@ from django.db import models
 # Criando modelo do Usuário
 
 class Usuario(models.Model):
-    nome = models.CharField(max_length=50)
-    email = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    cpf = models.CharField(max_length=15)
-    cep = models.CharField(max_length=10)
-    descricao = models.CharField(max_length=200)
-    numero = models.CharField(max_length=5)
-    cidade = models.CharField(max_length=20)
-    estado = models.CharField(max_length=20)
+    id_usuario = models.CharField( max_length=5)
+    nome = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=30, null=True)
+    password = models.CharField(max_length=30, null=True)
+    cpf = models.CharField(max_length=15, null=True)
+    cep = models.CharField(max_length=10, null=True)
+    descricao = models.CharField(max_length=200, null=True)
+    numero = models.CharField(max_length=5, null=True)
+    cidade = models.CharField(max_length=20, null=True)
+    estado = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.nome
